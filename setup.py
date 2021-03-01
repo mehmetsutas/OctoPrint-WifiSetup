04ab95831c306d84bc2e27ded136c6aff5d62054
Mehmet Sutaş <>
2021-03-01 19:55:13 +0300

    0.3

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ import octoprint_setuptools
 setuptools.setup(**octoprint_setuptools.create_plugin_setup_parameters(
 	identifier="wifisetup",
 	name="OctoPrint-Wifisetup",
-	version="0.2",
-	description="Plugin for recording Wifi psk through OctoPrint's interface. It's only available for Linux right now. Derived from Netconnectd plugin of Gina Häußge",
+	version="0.3",
+	description="Plugin for recording Wifi psk through OctoPrint's interface. It's only available for Debian Linux with nmcli right now. Derived from Netconnectd plugin of Gina Häußge",
 	author="Mehmet Sutas",
 	mail="msutas@rigid3d.com",
 	url="https://github.com/msutas/Wifisetup",
